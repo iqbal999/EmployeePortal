4509b10a7c35af941797881c11cec66ddd15552f
Md Iqbal Hossain <iqbal.hossain9@gmail.com>
2022-04-30 13:54:21 +0600

    Departments functionality complete

---
+++ EP.Server/wwwroot/js/common.js
@@ -0,0 +1,18 @@
+﻿
+function ShowDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('show');
+}
+
+function HideDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('hide');
+}
+
+window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, 'Success', { timeOut: 5000 })
+    }
+
+    if (type === "error") {
+        toastr.error(message, 'Error', { timeOut: 5000 })
+    }
+}
