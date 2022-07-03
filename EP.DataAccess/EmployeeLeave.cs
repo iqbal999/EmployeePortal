5890ca3f65d2ef5eb6a2197d4305ff82d135f856
Md Iqbal Hossain <iqbal.hossain9@gmail.com>
2022-07-03 16:02:18 +0600

    Add Gender field in employee entry

---
+++ EP.DataAccess/EmployeeLeave.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EP.DataAccess
+{
+    public class EmployeeLeave
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int EmployeeId { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee? Employee { get; set; }
+
+        public int LeaveId { get; set; }
+        [ForeignKey("LeaveId")]
+        public LeaveType? LeaveType { get; set; }
+
+        public double TotalLeaveDays { get; set; }
+        public double EnjoyLeaveDays { get; set; }
+        public double BalanceLeaveDays { get; set; }
+
+    }
+}
---
+++ EP.Models/EmployeeLeaveDTO.cs
@@ -0,0 +1,24 @@
+﻿using EP.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EP.Models
+{
+    public class EmployeeLeaveDTO
+    {
+        public int Id { get; set; }
+
+        public int EmployeeId { get; set; }
+        public Employee? Employee { get; set; }
+
+        public int LeaveId { get; set; }
+        public LeaveType? LeaveType { get; set; }
+
+        public double TotalLeaveDays { get; set; }
+        public double EnjoyLeaveDays { get; set; }
+        public double BalanceLeaveDays { get; set; }
+    }
+}
